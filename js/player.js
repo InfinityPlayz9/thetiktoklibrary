ed1747386df8dd57710e4268ae818c4f7bd3f088
GH0ZTY <56695928+GH0ZTYTV@users.noreply.github.com>
2021-07-04 22:02:37 +0200

    Add files via upload

--- js/player.js
+++ js/player.js
@@ -1,14 +1,36 @@
-$("#volume").slider({
-  min: 0,
-  max: 100,
-  value: 0,
-  range: "min",
-  slide: function (event, ui) {
-    setVolume(ui.value / 100);
-  }
+var myMedia = document.createElement('audio');
+$(document).ready(function() {
+    $("#volume").slider({
+        range: "min",
+        misn: 0,
+        max: 100,
+        value: 80,
+        animate: true,
+        slide: function(event, ui) {
+            setVolume((ui.value) / 100);
+        }
+    });
+
+    document.getElementById('player').appendChild(myMedia);
+    myMedia.id = "myMedia";
+    playMedia('ViewSource');
 });
 
+function playMedia(fileName) {
+
+
+    mediaExt = (myMedia.canPlayType('audio/mp3')) ? '.mp3' : (myMedia.canPlayType('audio/ogg')) ? '.ogg' : '';
+
+
+    if (mediaExt) {
+        myMedia.src = "http://planetoftheweb.com/exercises/jsuislider/audio/" + fileName + mediaExt;
+        myMedia.setAttribute('loop', 'loop');
+        myMedia.play();
+        setVolume(.8);
+    }
+}
+
 function setVolume(myVolume) {
-   var myMedia = document.getElementById('myMedia');
-   myMedia.volume = myVolume;
+    var myMedia = document.getElementById('myMedia');
+    myMedia.volume = myVolume;
 }
