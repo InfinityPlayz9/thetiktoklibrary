da12c59d442dd604ccd650bd896f4d9cb3da2a2f
GH0ZTY <56695928+GH0ZTYTV@users.noreply.github.com>
2021-07-05 04:06:08 +0200

    Menu

--- js/player.js
+++ js/player.js
@@ -1,36 +1,12 @@
-var myMedia = document.createElement('audio');
-$(document).ready(function() {
-    $("#volume").slider({
-        range: "min",
-        misn: 0,
-        max: 100,
-        value: 80,
-        animate: true,
-        slide: function(event, ui) {
-            setVolume((ui.value) / 100);
-        }
-    });
+var master = document.createElement('input');
+master.type = 'range';
+master.min = 0;
+master.max = 1;
 
-    document.getElementById('player').appendChild(myMedia);
-    myMedia.id = "myMedia";
-    playMedia('ViewSource');
-});
+master.onchange = function(event) {
+  var elements = document.querySelectorAll('audio, video');
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].volume = master.value;
+  }
+};
 
-function playMedia(fileName) {
-
-
-    mediaExt = (myMedia.canPlayType('audio/mp3')) ? '.mp3' : (myMedia.canPlayType('audio/ogg')) ? '.ogg' : '';
-
-
-    if (mediaExt) {
-        myMedia.src = "http://planetoftheweb.com/exercises/jsuislider/audio/" + fileName + mediaExt;
-        myMedia.setAttribute('loop', 'loop');
-        myMedia.play();
-        setVolume(.8);
-    }
-}
-
-function setVolume(myVolume) {
-    var myMedia = document.getElementById('myMedia');
-    myMedia.volume = myVolume;
-}
